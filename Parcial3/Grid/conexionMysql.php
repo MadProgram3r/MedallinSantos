8c3c075dd29e2a59e7ab4d8b2ae7b3e01752543d
MadProgram3r <oscar-ms8@hotmail.com>
2022-11-29 07:18:58 -0600

    Grid

--- Parcial3/Grid/conexionMysql.php
+++ Parcial3/Grid/conexionMysql.php
@@ -8,7 +8,7 @@ $servidor = "";
 $con = mysqli_connect($servidor, $usuario, $password, $baseDatos) or die("Problemas de conexion");
 $consulta = "SELECT * FROM pokemon";
 $registros = mysqli_query($con, $consulta);
-$result = mysqli_fetch_all($registros, MYSQL_ASSOC);
+$result = mysqli_fetch_all($registros, MYSQLI_ASSOC);
 
 
 mysqli_free_result($registros);
